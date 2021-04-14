66155e0e0f44ee32c278c0b47078c2ee37035b5c
Alexander Mead <alexander.j.mead@googlemail.com>
2021-04-14 10:10:24 +0100

    updates

--- mead_general.py
+++ mead_general.py
@@ -82,3 +82,8 @@ def loglog_InterpolatedUnivariateSpline(x, y, **kwargs):
 #def radians_to_degrees(theta):
 #    from numpy import pi
 #    return theta*180./pi
+
+# Check if a square array is symmetric
+def check_symmetric(a, rtol=1e-05, atol=1e-08):
+    from numpy import allclose
+    return allclose(a, a.T, rtol=rtol, atol=atol)
