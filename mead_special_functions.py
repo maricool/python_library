f2c6dfd51f8f9d1d58681439299705e016cf57ff
Alexander Mead <alexander.j.mead@googlemail.com>
2021-05-18 11:27:23 +0100

    tidy

--- mead_special_functions.py
+++ mead_special_functions.py
@@ -1,26 +1,28 @@
 import numpy as np
 
-# Tophat Fourier transform function
-# Normalised such that T(x=0)=1
+# Parameters
+xmin_Tk = 1e-5
+
 def Tophat_k(x):
-    xmin=1e-5
+    # Normalised tophat Fourier transform function such that T(x=0)=1
+    xmin = xmin_Tk
     return np.where(np.abs(x)<xmin, 1.-x**2/10., (3./x**3)*(np.sin(x)-x*np.cos(x)))
 
-# Gaussian function
-# Normalised such that G(x=0)=1
-# mu - mean
-# sig - standard deviation
 def Gaussian(x, mu, sig):
+    # Normalised Gaussian function such that G(x=0)=1
+    # NOTE: The integral over this will not be unity
+    # mu - mean
+    # sig - standard deviation
     return np.exp(-(x-mu)**2/(2.*sig**2))
 
-# Kronecker delta function
 def KroneckerDelta(x1, x2):
-    return np.where(x1==x2, 1., 0.)
+    # Kronecker delta function
+    return np.where(x1==x2, 1, 0)
 
-# Cube root
 def cbrt(x):
+    # Cube root
     return x**(1./3.)
 
-# Logarithm to the base n
 def logn(x, n):
+    # Logarithm to the base n
     return np.log(x)/np.log(n)
