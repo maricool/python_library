ef41634f9e7e1a2cd4f84eef31268dcc7c04bd17
Alexander Mead <alexander.j.mead@googlemail.com>
2022-03-16 09:23:51 -0700

    reorganize and deleted mead_numpy; numpy functions in general

--- mead_general.py
+++ mead_general.py
@@ -1,15 +1,26 @@
+### Functions ###
+# First set involve only basic Python, then numpy, then matplotlib
+
 ### This set of functions use only basic Python ###
 
 def opposite_side(left_or_right):
+    '''
+    Returns the opposite of the strings: 'left' or 'right'
+    '''
     if left_or_right == 'left':
         out = 'right'
     elif left_or_right == 'right':
         out = 'left'
     else:
-        raise ValueError('Input should be left or right')
+        raise ValueError('Input should be either \'left\' or \'right\'')
     return out
 
 def number_name(n):
+    '''
+    The standard name for 10^n
+    e.g., 10^2 = hundred
+    e.g., 10^9 = billion
+    '''
     if n == int(1e1):
         return 'ten'
     elif n == int(1e2):
@@ -26,7 +37,6 @@ def number_name(n):
         return 'billion'
     else:
         raise ValueError('Integer does not appear to have a name')
-        #return None
 
 def file_length(fname):
     '''
@@ -39,15 +49,23 @@ def file_length(fname):
             pass
     return i+1
 
-def mrange(a, b=None):
+def mrange(a, b=None, step=None):
     '''
     Mead range is more sensible than range, goes (1, 2, ..., n)
+    e.g., list(range(4)) = [1, 2, 3, 4]
+    e.g., list(range(2, 4)) = [2, 3, 4]
     I hate the inbuilt Python 'range' with such fury that it frightens me
     '''
-    if b is None:
-        return range(a+1)
+    if step is None:
+        if b is None:
+            return range(a+1)
+        else:
+            return range(a, b+1)
     else:
-        return range(a, b+1)
+        if b is None:
+            raise ValueError('If a step is specified then you must also specify start (a) and stop (b)')
+        else:
+            return range(a, b+1, step)
 
 def is_float_close_to_integer(x):
     '''
@@ -55,6 +73,20 @@ def is_float_close_to_integer(x):
     '''
     from math import isclose
     return isclose(x, int(x))
+
+def bin_edges_for_integers(a, b=None):
+    '''
+    Defines a set of bin edges for histograms of integer data
+    e.g., bin_edges_for_integers(1, 5) = 0.5, 1.5, 2.5, 3.5, 4.5, 5.5 so that integers are at centres
+    e.g., bin_edges_for_integers(a) assumes a is array and goes from min-0.5 to max+0.5 in steps of unity
+    '''
+    if b is None:
+        bin_edges = list(mrange(min(a), max(a)+1))
+    else:
+        bin_edges = list(mrange(a, b+1))
+    bin_edges = [bin_edge-0.5 for bin_edge in bin_edges] # Centre on integers
+    return bin_edges
+
 
 ### ###
 
@@ -74,6 +106,21 @@ def remove_list_from_list(removal_list, original_list):
     for item in removal_list:
         if item in original_list:
             original_list.remove(item)
+
+def second_largest(numbers):
+    '''
+    Returns the second-largest entry in collection of numbers
+    '''
+    count = 0
+    m1 = m2 = float('-inf')
+    for x in numbers:
+        count += 1
+        if x > m2:
+            if x >= m1:
+                m1, m2 = x, m1
+            else:
+                m2 = x
+    return m2 if count >= 2 else None
 
 ### ###
 
@@ -95,12 +142,6 @@ def logspace(xmin, xmax, nx):
     from numpy import logspace, log10
     return logspace(log10(xmin), log10(xmax), nx)
 
-def is_power_of_two(x):
-    '''
-    True if arguement is a perfect power of two
-    '''
-    return is_power_of_n(x, 2)
-
 def is_power_of_n(x, n):
     '''
     Checks if x is a perfect power of n (e.g., 32 = 2^5)
@@ -108,6 +149,12 @@ def is_power_of_n(x, n):
     from mead_special_functions import logn
     lg = logn(x, n)
     return is_float_close_to_integer(lg)
+
+def is_power_of_two(x):
+    '''
+    True if arguement is a perfect power of two
+    '''
+    return is_power_of_n(x, 2)
 
 def is_perfect_square(x):
     '''
@@ -136,21 +183,23 @@ def print_array_attributes(x):
     print('dtype:', x.dtype)
     print('')
 
-def print_full_array(xs):
+def print_full_array(xs, title=None):
     '''
     Print full array(-like) to screen with indices
     '''
+    if title is not None:
+        print(title)
     for ix, x in enumerate(xs):
         print(ix, x)
 
-def nans(shape, **kwargs):
+def array_of_nans(shape, **kwargs):
     '''
     Initialise an array of nans
     '''
     from numpy import empty, nan
     return nan*empty(shape, **kwargs)
 
-def remove_nans(x):
+def remove_nans_from_array(x):
     '''
     Remove nans from array x
     '''
@@ -170,6 +219,57 @@ def array_values_at_indices(array, list_of_array_positions):
     else:
         ValueError('Error, this only works in either one or two dimensions at the moment')
         return None
+
+def print_array_statistics(x):
+    '''
+    Print useful array statistics
+    '''
+    from numpy import sqrt
+    print('Array statistics')
+    n = x.size
+    print('size:', n)
+    print('sum:', x.sum())
+    print('min:', x.min())
+    print('max:', x.max())
+    mean = x.mean()
+    print('mean:', mean)
+    std = x.std()
+    var = std**2
+    #std_bc = x.std(ddof=1) # Avoiding this to prevent multiple similar calculations
+    std_bc = std*sqrt(n/(n-1))
+    var_bc = std_bc**2
+    print('std:', std)
+    print('std (Bessel corrected):', std_bc)
+    print('variance:', var)
+    print('variance (Bessel corrected):', var_bc**2)
+    print('<x^2>:', mean**2+var)
+    print()
+
+def standardize_array(x):
+    '''
+    From: https://towardsdatascience.com/pca-with-numpy-58917c1d0391
+    This function standardizes an array, its substracts mean value, 
+    and then divide the standard deviation.
+        x: array 
+        return: standardized array
+    '''
+    import numpy as np
+    rows, columns = x.shape
+    
+    standardizedArray = np.zeros(shape=(rows, columns))
+    tempArray = np.zeros(rows)
+    
+    for col in range(columns):
+        mean = np.mean(x[:, col])
+        std = np.std(x[:, col])
+        tempArray = np.empty(0)
+        
+        for element in x[:, col]:
+            tempArray = np.append(tempArray, (element-mean)/std)
+ 
+        standardizedArray[:, col] = tempArray
+    
+    return standardizedArray
 
 # use numpy deg2rad() or radians()
 #def degrees_to_radians(theta):
